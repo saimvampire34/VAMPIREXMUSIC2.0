025d6e0928ce45b586cf686de19ab6b17c922a79
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-12-15 04:00:15 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -11,7 +11,7 @@ from ..logging import LOGGER
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("-1002090555194")
+TEST_ID = int("-1002292589408")
 
 assistants = []
 assistantids = []
